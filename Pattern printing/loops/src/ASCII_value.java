1d11a310dd69225f028dc3cb2924e45f281b2146
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-04-17 01:39:59 +0530

    new file:   .vscode/launch.json new file:   intro/Main.java

---
+++ Pattern printing/loops/src/ASCII_value.java
@@ -0,0 +1,9 @@
+public class ASCII_value {
+    public static void main(String[] args) {
+      int n = 65;
+      while(n<=91){
+          System.out.println( n + " "+ (char)n );
+          n++;
+      }
+    }
+}
